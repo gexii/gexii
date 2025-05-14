6a4d2f19685d9598a7b25fe66dadf641e3c2155a
Zdd Zhong <zz997106@gmail.com>
2025-05-14 23:39:25 +0800

    feat: Return action as a function in useAction hook

--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -18,42 +18,57 @@ export function useAction<
   const state = useMemo(() => {
     return { loading, error, data };
   }, [loading, error, data]);
+
   const stateRef = useRef(state);
   stateRef.current = state;
 
   const callbackRef = useRef(callback);
   callbackRef.current = callback;
 
+  const optionsRef = useRef(options);
+  optionsRef.current = options;
+
+  // ----- FUNCTIONS -----
+
+  const call = async (...args: Parameters<T>) => {
+    setLoading(true);
+    setError(null);
+    const options = optionsRef.current;
+
+    try {
+      const result = await callbackRef.current(...args);
+      setData(result);
+      setError(null);
+      options.onSuccess?.(result);
+      return result;
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+        options.onError?.(error);
+
+        if (options.throwOnError !== false) throw error;
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setLoading(false);
+    setError(null);
+    setData(defaultValue);
+  };
+
   return useMemo(() => {
-    return {
-      isLoading: () => stateRef.current.loading,
+    const action: Action<T, TDefault> = (...args) => call(...args);
+    action.isLoading = () => stateRef.current.loading;
+    action.getError = () => stateRef.current.error;
+    action.getData = () => stateRef.current.data;
+    action.hasError = () => !!stateRef.current.error;
+    action.call = (...args) => call(...args);
+    action.reset = () => reset();
 
-      getError: () => stateRef.current.error,
-
-      getData: () => stateRef.current.data,
-
-      hasError: () => !!stateRef.current.error,
-
-      call: (async (...args: Parameters<T>) => {
-        setLoading(true);
-        setError(null);
-        try {
-          const result = await callbackRef.current(...args);
-          setData(result);
-          setError(null);
-          options.onSuccess?.(result);
-        } catch (error) {
-          if (error instanceof Error) {
-            setError(error);
-            options.onError?.(error);
-
-            if (options.throwOnError !== false) throw error;
-          }
-        } finally {
-          setLoading(false);
-        }
-      }) as T,
-    };
+    return action;
   }, []);
 }
 
@@ -63,7 +78,9 @@ export interface Action<
   T extends (...args: any[]) => any,
   TDefault extends Awaited<ReturnType<T>> | null,
 > {
-  call: T;
+  (...args: Parameters<T>): Promise<Awaited<ReturnType<T>>>;
+  call(...args: Parameters<T>): Promise<Awaited<ReturnType<T>>>;
+  reset(): void;
   getError(): Error | null;
   getData(): Awaited<ReturnType<T>> | TDefault;
   isLoading(): boolean;
